2fbd34a3e5817e93b624dbd9dd65aa175016b529
ZruPanda <arjiansyah3@gmail.com>
2023-10-12 11:17:43 +0700

    My Second commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,7 @@
  */
 package test;
 
+import test.Second;
 /**
  *
  * @author admin
@@ -18,6 +19,8 @@ public class Test {
         // TODO code application logic here
         System.out.println("Hello World");
         System.out.println("First Commit");
+        
+        Second.cetak2();
     }
     
 }
